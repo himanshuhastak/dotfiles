831f18226265f6bdff151438b9dab4b52c5d251e
HIMANSHU HASTAK <hastakh@hastakh.internal.synopsys.com>
2018-12-11 14:05:34 +0530

    change to softlink instead of source

--- initEnvt.sh
+++ initEnvt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
-echo "source ~/GIT/dotfiles/bash_aliases"       >| ~/.bash_aliases
-echo "source ~/GIT/dotfiles/bashrc.hastakh"     >| ~/.bashrc.hastakh
-echo "source ~/GIT/dotfiles/vimrc"              >| ~/.vimrc
-echo "source ~/GIT/dotfiles/tmux.conf"          >| ~/.tmux.conf
-echo "source ~/GIT/dotfiles/taskrc"             >| ~/.taskrc
+ln -s ~/GIT/dotfiles/bash_aliases        ~/.bash_aliases
+ln -s ~/GIT/dotfiles/bashrc.hastakh      ~/.bashrc.hastakh
+ln -s ~/GIT/dotfiles/vimrc               ~/.vimrc
+ln -s ~/GIT/dotfiles/tmux.conf           ~/.tmux.conf
+ln -s ~/GIT/dotfiles/taskrc              ~/.taskrc
