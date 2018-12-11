43b43c56806fde3f2f2468062bb6b74c86675675
HIMANSHU HASTAK <hastakh@hastakh.internal.synopsys.com>
2018-12-11 14:12:06 +0530

    force syn link creation

--- initEnvt.sh
+++ initEnvt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
-ln -s ~/GIT/dotfiles/bash_aliases        ~/.bash_aliases
-ln -s ~/GIT/dotfiles/bashrc.hastakh      ~/.bashrc.hastakh
-ln -s ~/GIT/dotfiles/vimrc               ~/.vimrc
-ln -s ~/GIT/dotfiles/tmux.conf           ~/.tmux.conf
-ln -s ~/GIT/dotfiles/taskrc              ~/.taskrc
+ln -nfs ~/GIT/dotfiles/bash_aliases        ~/.bash_aliases
+ln -nfs ~/GIT/dotfiles/bashrc.hastakh      ~/.bashrc.hastakh
+ln -nfs ~/GIT/dotfiles/vimrc               ~/.vimrc
+ln -nfs ~/GIT/dotfiles/tmux.conf           ~/.tmux.conf
+ln -nfs ~/GIT/dotfiles/taskrc              ~/.taskrc
