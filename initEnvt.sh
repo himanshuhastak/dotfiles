f947562f0d8d29f8e5b318955a0be7ef2cd42d55
hastakh <hastakh@synopsys.com>
2022-05-24 03:00:39 -0700

    change ssh settings

--- initEnvt.sh
+++ initEnvt.sh
@@ -7,3 +7,16 @@ ln -nfs ~/GIT/dotfiles/tmux.conf           ~/.tmux.conf
 ln -nfs ~/GIT/dotfiles/taskrc              ~/.taskrc
 mkdir -p ~/bin
 ln -nfs ~/GIT/dotfiles/rsync.sh            ~/bin/rsync.sh
+
+#### SSH stuff
+chmod g-w ~
+mkdir -p ~/.ssh/
+chmod 744 ~/.ssh
+touch ~/.ssh/authorized_keys
+chmod 700 ~/.ssh
+chmod 755 ~/.ssh/authorized_keys
+#chmod 640 ~/.ssh/authorized_keys
+chmod 644 ~/.ssh/config
+chmod 600 ~/.ssh/id_rsa
+#chmod ~ 755 
+
